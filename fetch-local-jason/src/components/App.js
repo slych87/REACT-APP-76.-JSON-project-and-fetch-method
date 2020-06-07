fa7ca35553442f2b2e66b327691cc4d5147bd059
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-07 12:05:54 +0200

    lesson done

--- fetch-local-jason/src/components/App.js
+++ fetch-local-jason/src/components/App.js
@@ -4,31 +4,42 @@ import Word from './Word';
 
 class App extends Component {
   state = {
-    words: [
-      {
-        "id": 1,
-        "en": "cat",
-        "pl": "kot"
-      },
-      {
-        "id": 2,
-        "en": "dog",
-        "pl": "pies"
-      },
-      {
-        "id": 3,
-        "en": "fish",
-        "pl": "ryba"
-      }
-    ] /*Zrobimy żądanie AJAX - asynchroniczne żądanie */
+    words: [], /*Zrobimy żądanie AJAX - asynchroniczne żądanie */
+    isLoaded: false,
   }
+
+  componentDidMount() {
+    // setTimeout(this.fetchData, 3000)
+
+    fetch('data/words.json') // poobieramy dane, ale jest ich więcej niż byśmy chcieli
+      .then(response => response.json()) // przerabiamy je na JSON, CZYLI WYODRĘBNIAMY
+      .then(data => {
+        this.setState({
+          words: data.words, //dostajemy się w pliku JSON do obiektu words
+          isLoaded: true,
+        })
+      })
+
+  }
+
+  // fetchData = () => {
+  //   fetch('data/words.json') // poobieramy dane, ale jest ich więcej niż byśmy chcieli
+  //     .then(response => response.json()) // przerabiamy je na JSON, CZYLI WYODRĘBNIAMY
+  //     .then(data => {
+  //       this.setState({
+  //         words: data.words, //dostajemy się w pliku JSON do obiektu words
+  //         isLoaded: true,
+  //       })
+  //     })
+  // }
+
   render() {
     const words = this.state.words.map(word => (
       <Word key={word.id} english={word.en} polish={word.pl} />
     ))
     return (
       <ul >
-        {words}
+        {this.state.isLoaded ? words : "Wczytuję dane"}
       </ul>
     );
   }
